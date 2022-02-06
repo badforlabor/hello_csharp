71fb0fb5f9f377bea49e03602c6c7f655147310c
liubo-work <liubo@work.com>
2022-02-06 13:13:33 +0800

    test equal

---
+++ test_equals/test_equals/MyHashTool.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace libre_hash
+{
+    public class MyHashTool
+    {
+        // 计算一堆对象的hash
+        public static int CombineHash(params object[] objs)
+        {
+            if (objs == null || objs.Length == 0)
+            {
+                return 0;
+            }
+
+            var vals = new int[objs.Length];
+            for(int i=0; i<objs.Length; i++)
+            {
+                vals[i] = GetHashCode(objs[i]);
+            }
+            return CalcHash(vals);
+        }
+
+        // 计算各种类型的hashcode
+        public static int GetHashCode(object obj)
+        {
+            if (obj == null)
+            {
+                return 0;
+            }
+
+            if (obj is string)
+            {
+                return (obj as string).GetHashCode();
+            }
+
+            // Dict
+            if (obj is System.Collections.IDictionary)
+            {
+                var d = obj as System.Collections.IDictionary;
+                return CalcHash(GetHashCode(d.Keys), GetHashCode(d.Values));
+            }
+
+            // 列表
+            if (obj is System.Collections.IEnumerable)
+            {
+                var e = obj as System.Collections.IEnumerable;
+                var vals = new List<int>();
+                foreach (var it in e)
+                {
+                    var h = GetHashCode(it);
+                    vals.Add(h);
+                }
+                return CalcHash(vals.ToArray());
+            }
+
+            return obj.GetHashCode();
+        }
+
+        // 各种类型的Equals
+        public new static bool Equals(object obj1, object obj2)
+        {
+            if (obj1 == null && obj2 == null)
+            {
+                return true;
+            }
+
+            if (obj1 == null)
+            {
+                return false;
+            }
+
+            if (obj2 == null)
+            {
+                return false;
+            }
+
+            if (obj1.GetType() != obj2.GetType())
+            {
+                return false;
+            }
+
+
+            if (obj1 is System.Collections.IDictionary && obj2 is System.Collections.IDictionary)
+            {
+                var d1 = obj1 as System.Collections.IDictionary;
+                var d2 = obj2 as System.Collections.IDictionary;
+
+                return Equals(d1.Keys, d2.Keys)
+                    && Equals(d1.Values, d2.Values);
+            }
+
+                // 列表
+            if (obj1 is System.Collections.IEnumerable && obj2 is System.Collections.IEnumerable)
+            {
+                System.Collections.IList aList = new List<object>();
+                System.Collections.IList bList = new List<object>();
+                if (obj1 is System.Collections.IList && obj2 is System.Collections.IList)
+                {
+                    aList = obj1 as System.Collections.IList;
+                    bList = obj2 as System.Collections.IList;
+                }
+                else
+                {
+                    foreach (var it in obj1 as System.Collections.IEnumerable)
+                    {
+                        aList.Add(it);
+                    }
+                    foreach (var it in obj2 as System.Collections.IEnumerable)
+                    {
+                        bList.Add(it);
+                    }
+                }
+
+                if (aList.Count != bList.Count)
+                {
+                    return false;
+                }
+
+                for (int i = 0; i < aList.Count; i++)
+                {
+                    if (!Equals(aList[i], bList[i]))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            return obj1.Equals(obj2);
+        }
+
+        // 计算hash-vals的结果
+        public static int CalcHash(params int[] vals)
+        {
+            return CalcHash(17, 31, vals);
+        }
+
+        public static int CalcHash(int seed, int factor, params int[] vals)
+        {
+            unchecked
+            {
+                int hash = seed;
+                foreach (int i in vals)
+                {
+                    hash = (hash * factor) + i;
+                }
+                return hash;
+            }
+        }
+    }
+}
---
+++ test_equals/test_equals/Program.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using libre_hash;
+
+namespace test_equals
+{
+
+    public class CA
+    {
+        public int A;
+        public float B;
+        public long C;
+        public ulong D;
+        public byte E;
+        public string F;
+
+        public static CA New()
+        {
+            return new CA() { A=1, B=2.2f, C=3, D=4, E=5, F = "F" };
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as CA;
+
+            return A.Equals(other.A)
+                && B.Equals(other.B)
+                && C.Equals(other.C)
+                && D.Equals(other.D)
+                && E.Equals(other.E)
+                && F.Equals(other.F);
+        }
+
+        public override int GetHashCode()
+        {
+            return MyHashTool.CombineHash(A, B, C, D, E, F);            
+        }
+    }
+
+    public struct SA
+    {
+        public int A;
+        public float B;
+        public long C;
+        public ulong D;
+        public byte E;
+        public string F;
+
+
+        public static SA New()
+        {
+            return new SA() { A = 1, B = 2.2f, C = 3, D = 4, E = 5, F = "F" };
+        }
+
+        // 纯结构体，不用写equals
+        //public override bool Equals(object obj)
+        //{
+        //    SA other = (SA)obj;
+
+        //    return A.Equals(other.A)
+        //        && B.Equals(other.B)
+        //        && C.Equals(other.C)
+        //        && D.Equals(other.D)
+        //        && E.Equals(other.E)
+        //        && F.Equals(other.F);
+        //}
+
+        // 但是需要写GetHashCode？
+        public override int GetHashCode()
+        {
+            return MyHashTool.CombineHash(A, B, C, D, E, F);
+        }
+    }
+    public struct SB
+    {
+        public int A;
+        public float B;
+        public long C;
+        public ulong D;
+        public byte E;
+        public string F;
+
+
+        public static SB New()
+        {
+            return new SB() { A = 1, B = 2.2f, C = 3, D = 4, E = 5, F = "F" };
+        }
+
+        public override bool Equals(object obj)
+        {
+            SB other = (SB)obj;
+
+            return A.Equals(other.A)
+                && B.Equals(other.B)
+                && C.Equals(other.C)
+                && D.Equals(other.D)
+                && E.Equals(other.E)
+                && F.Equals(other.F);
+        }
+
+        public override int GetHashCode()
+        {
+            return MyHashTool.CombineHash(A, B, C, D, E, F);
+        }
+    }
+
+    public class CB
+    {
+        public CA A;
+        public SA B;
+
+        public List<CA> C;
+        public List<SA> D;
+
+        public Dictionary<string, CA> E;
+        public Dictionary<string, SA> F;
+
+        public static CB New()
+        {
+            return new CB()
+            {
+                A = CA.New(),
+                B = SA.New(),
+                C = new List<CA>() { CA.New(), CA.New() },
+                D = new List<SA>() { SA.New(), SA.New() },
+                E = new Dictionary<string, CA>() { {"A", CA.New() }, { "B", CA.New() }, },
+                F = new Dictionary<string, SA>() { { "sA", SA.New() }, { "sB", SA.New() }, },
+            };
+        }
+
+        public override bool Equals(object obj)
+        {
+            CB other = (CB)obj;
+
+            return A.Equals(other.A)
+                && B.Equals(other.B)
+                && MyHashTool.Equals(C, other.C)
+                && MyHashTool.Equals(D, other.D)
+                && MyHashTool.Equals(E, other.E)
+                && MyHashTool.Equals(F, other.F);
+        }
+
+        public override int GetHashCode()
+        {
+            return MyHashTool.CombineHash(A, B, C, D, E, F);
+        }
+    }
+
+
+    class Program
+    {
+        static void Test1()
+        {
+            var ca = CA.New();
+            var caHash = ca.GetHashCode();
+            Console.WriteLine($"{caHash}");
+
+            var cb = CA.New();
+            Debug.Assert(cb.GetHashCode() == ca.GetHashCode());
+            Debug.Assert(cb.Equals(ca));
+
+            var cc = CA.New();
+            cc.F = "FF";
+            Debug.Assert(cc.GetHashCode() != ca.GetHashCode());
+            Debug.Assert(!cc.Equals(ca));
+
+            var cd = CA.New();
+            cd.B = 2;
+            Debug.Assert(cd.GetHashCode() != ca.GetHashCode());
+            Debug.Assert(!cd.Equals(ca));
+        }
+        static void Test2()
+        {
+            var ca = SA.New();
+            var caHash = ca.GetHashCode();
+            Console.WriteLine($"{caHash}");
+
+            var cb = SA.New();
+            Debug.Assert(cb.GetHashCode() == ca.GetHashCode());
+            Debug.Assert(cb.Equals(ca));
+
+            var cc = SA.New();
+            cc.F = "FF";
+            Debug.Assert(cc.GetHashCode() != ca.GetHashCode());
+            Debug.Assert(!cc.Equals(ca));
+
+            var cd = SA.New();
+            cd.B = 2;
+            Debug.Assert(cd.GetHashCode() != ca.GetHashCode());
+            Debug.Assert(!cd.Equals(ca));
+        }
+        static void Test3()
+        {
+            var ca = CB.New();
+            var caHash = ca.GetHashCode();
+            Console.WriteLine($"{caHash}");
+
+            var cb = CB.New();
+            Debug.Assert(cb.GetHashCode() == ca.GetHashCode());
+            Debug.Assert(cb.Equals(ca));
+        }
+
+
+        static void Main(string[] args)
+        {
+            Test1();
+            Test2();
+            Test3();
+        }
+
+    }
+}
