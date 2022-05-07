dbeaf0f4be0fc6071766c7c71b745c4825b10236
liubo-work <liubo@work.com>
2022-05-07 15:51:42 +0800

    更多例子

---
+++ test_operator/test_operator/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_operator
+{
+    public struct OpSample
+    {
+        public int Val;
+        
+        public OpSample(int val)
+        {
+            this.Val = val;
+        }
+
+        public static OpSample operator --(OpSample s)
+        {
+            s.Val = --s.Val;
+            return s;
+        }
+
+        //Overload unary increment operator
+        public static OpSample operator ++(OpSample s)
+        {
+            s.Val = ++s.Val;
+            return s;
+        }
+
+        public void PrintValues()
+        {
+            Console.WriteLine("Values of val: " + Val);
+            Console.WriteLine();
+        }
+    }
+
+    class Program
+    {
+
+
+
+
+        static void Main(string[] args)
+        {
+            OpSample s = new OpSample();
+
+            s++;
+            s++;
+            s++;
+            System.Diagnostics.Debug.Assert(s.Val == 3);
+
+            s--;
+            s--;
+            s--;
+            System.Diagnostics.Debug.Assert(s.Val == 0);
+        }
+    }
+}
