1049207fd385935bac4b171c2ae27e4c0f05e2f6
liubo-work <liubo@work.com>
2022-07-18 09:41:20 +0800

    1

---
+++ test_delegate/test_delegate/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test1 = new Test1();
+            test1.evt += Test1_evt;
+
+            test1.Call();
+        }
+
+        private static void Test1_evt(int obj)
+        {
+
+        }
+    }
+    class Test1
+    {
+        public event System.Action<int> evt;
+
+        public void Call()
+        {
+            evt?.Invoke(1);
+        }
+    }
+}
---
+++ test_string/test_string/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var keep = "".ToArray();
+            Console.WriteLine(keep);
+
+            var s = Proc("Abc中文def_xx@123");
+            Console.WriteLine(s);
+        }
+
+        public static string Proc(string s)
+        {
+            //if (!StringUtils.IsSnakeCase(s))
+            {
+                // 譬如 CamelCase -> camelcase
+                var arr = s.ToCharArray();
+                var sb = new StringBuilder();
+                foreach (var c in arr)
+                {
+                    if (c >= 'A' && c <= 'Z')
+                    {
+                        var c2 = c - 'A' + 'a';
+                        sb.Append((char)c2);
+                    }
+                    else if (c >= 'a' && c <= 'z')
+                    {
+                        sb.Append(c);
+                    }
+                    else if (c >= '0' && c <= '9')
+                    {
+                        sb.Append(c);
+                    }
+                    else if (c == '@')
+                    {
+                        sb.Append(c);
+                    }
+                    else if (c == '_')
+                    {
+                        sb.Append(c);
+                    }
+                    else
+                    {
+                        sb.Append('_');
+                    }
+                }
+
+                return sb.ToString();
+            }
+
+            return "";
+        }
+    }
+}
